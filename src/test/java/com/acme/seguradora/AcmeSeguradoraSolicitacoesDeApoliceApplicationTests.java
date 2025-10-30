843344604041592ae443e6ec232aa5ce1de76e1d
Fernando Andrade Oliveira <oliveira.desenv@gmail.com>
2025-10-30 16:49:41 -0300

    Add files via upload

---
+++ src/test/java/com/acme/seguradora/AcmeSeguradoraSolicitacoesDeApoliceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.seguradora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeSeguradoraSolicitacoesDeApoliceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
